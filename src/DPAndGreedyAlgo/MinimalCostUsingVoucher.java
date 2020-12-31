77942987224259d40729c35c8a6b509fd89336a0
yuaohan <u6799528@anu.edu.au>
2021-01-01 02:19:12 +1100

    update solution of MinimalCostUsingVoucher, in which Integer.MAX_VALUE could be used in a proper way for representing that there is no such a way.

--- src/DPAndGreedyAlgo/MinimalCostUsingVoucher.java
+++ src/DPAndGreedyAlgo/MinimalCostUsingVoucher.java
@@ -26,7 +26,7 @@ public class MinimalCostUsingVoucher {
 		int money = in.nextInt();
 		int[] arr = new int[len];
 
-		for(int i=0;i<len;i++) {
+		for (int i = 0; i < len; i++) {
 			arr[i] = in.nextInt();
 		}
 
@@ -34,37 +34,39 @@ public class MinimalCostUsingVoucher {
 
 	}
 
-	private static int helper(int[] arr, int len, int money){
+	private static int helper(int[] arr, int len, int money) {
 
 		// 选择编号范围在[0,i]的菜，要凑起价格至少为j元钱，最少花多少钱
-		int[][] dp = new int[len][money+1];
+		int[][] dp = new int[len][money + 1];
 
-		for(int i=0;i<=money;i++){
+		for (int i = 0; i <= money; i++) {
 			// 举例：0号菜价格为5元。意思是如果要只能选0号菜，你的选择只有买0号菜和不买0号菜，
 			// 要凑起价格至少为0或1或2或3或4或5元的菜，都至少花费5元，即0号菜必须买
-			if(i<=arr[0]){
+			if (i <= arr[0]) {
 				dp[0][i] = arr[0];
 			}
 
 			// 如果要花费至少在5元以上，如6元，7元……，是不可能的，因为所有菜全买（即买0号菜），也只能凑出5元，
 			// 所以答案是不存在这种情况
-			else{
-				// 这里一开始用的是Integer.MAX_VALUE,不行，会超边界，改掉就通过了
-				dp[0][i] = 10001;
+			else {
+				dp[0][i] = Integer.MAX_VALUE;
 			}
 		}
 
-		for(int i=1;i<len;i++){
-			for(int j=1;j<=money;j++){
-				if(j<arr[i]){
-					dp[i][j] = Math.min(arr[i], dp[i-1][j]);
-				}
-				else{
-					dp[i][j] = Math.min(dp[i-1][j], arr[i] + dp[i-1][j-arr[i]]);
+		for (int i = 1; i < len; i++) {
+			for (int j = 1; j <= money; j++) {
+				if (j < arr[i]) {
+					dp[i][j] = Math.min(arr[i], dp[i - 1][j]);
+				} else {
+					if (dp[i - 1][j - arr[i]] == Integer.MAX_VALUE) {
+						dp[i][j] = dp[i - 1][j];
+					} else {
+						dp[i][j] = Math.min(dp[i - 1][j], arr[i] + dp[i - 1][j - arr[i]]);
+					}
 				}
 			}
 		}
 
-		return dp[len-1][money];
+		return dp[len - 1][money];
 	}
 }
