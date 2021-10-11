019b2bebe53f3ecb681b943ec367e608adfa9858
yuaohan001 <yuaohan001@ke.com>
2021-10-11 17:21:46 +0800

    死锁例子，Thread类的start方法和run方法比较

--- src/concurrency/practice/DeadLockPractice.java
+++ src/concurrency/practice/DeadLockPractice.java
@@ -4,4 +4,53 @@ package concurrency.practice;
  * 实现一个死锁的例子，顺便讲一下什么是死锁
  */
 public class DeadLockPractice {
+	static final Object objA = new Object();
+	static final Object objB = new Object();
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "准备获取objA资源");
+				synchronized (objA){
+					System.out.println(Thread.currentThread().getName() + "现在获取到了objA资源");
+
+					try {
+						Thread.sleep(1000L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					System.out.println(Thread.currentThread().getName() + "准备获取objB资源");
+					synchronized (objB){
+						System.out.println(Thread.currentThread().getName() + "现在获取到了objB资源");
+					}
+				}
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "准备获取objB资源");
+				synchronized (objB){
+					System.out.println(Thread.currentThread().getName() + "现在获取到了objB资源");
+
+					try {
+						Thread.sleep(1000L);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					System.out.println(Thread.currentThread().getName() + "准备获取objA资源");
+					synchronized (objA){
+						System.out.println(Thread.currentThread().getName() + "现在获取到了objA资源");
+					}
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+	}
 }
---
+++ src/concurrency/practice/ThreadStartAndRunDemo.java
@@ -0,0 +1,30 @@
+package concurrency.practice;
+
+/**
+ * Thread类中.start()方法和.run()方法的区别
+ */
+public class ThreadStartAndRunDemo {
+	/**
+	 * block A和block B相互替换，使用不同的block得到的结果不同。
+	 * 调用run方法，仍然使用主线程在执行任务，仍然是同步的，所以会先执行run方法，打印pong,再打印main方法中的ping
+	 * 调用start方法，是真正的多线程操作，生成一个新线程，主线程返回，继续执行主线程的任务，由新线程去执行run()里的任务，
+	 * 所以主线程先执行打印ping,然后新线程执行t1对象的run方法，打印pong
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("pong");
+			}
+		});
+
+		// block A
+		t1.run();
+		System.out.println("ping");
+
+		// block B
+//		t1.start();
+//		System.out.println("ping");
+	}
+}
